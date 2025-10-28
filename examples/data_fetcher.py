034e333b39e3bd3469d654be3fb5b016b5e21ad3
Fateme <fatemetaklifi@gmail.com>
2025-10-28 17:06:34 +0330

    Update data_fetcher.py

--- examples/data_fetcher.py
+++ examples/data_fetcher.py
@@ -3,5 +3,5 @@ from current_temp_API.api import OpenMeteo, OpenWeather
 open_meteo_obj = OpenMeteo(52.52, 13.41)
 print(str(open_meteo_obj.get_current_temperature()) + " C")
 
-open_weather_obj = OpenWeather(52.52, 13.41, api_token="68201ab01d21b62cb523e0f86f0aa8f3")
+open_weather_obj = OpenWeather(52.52, 13.41, api_token="") # api_token = open weather api token (free)
 print(str(open_weather_obj.get_current_temperature()) + " K")
